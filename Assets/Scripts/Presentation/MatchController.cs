c87f0adcf2079bc7a91956e7c9991f8ede67bd11
vitorjacintho <vitor.jacintho@bairesdev.com>
2020-08-27 23:18:28 -0300

    Moves added columns left, cleanup of packages

--- Assets/Scripts/Presentation/MatchController.cs
+++ Assets/Scripts/Presentation/MatchController.cs
@@ -1,6 +1,8 @@
 using System;
 using Domain.Entities;
+using Unity.Burst;
 using UnityEngine;
+using UnityEngine.Jobs;
 
 namespace DefaultNamespace
 {
@@ -10,16 +12,13 @@ namespace DefaultNamespace
         public Transform spawnPoint;
         public OrePrefabMap[] orePrefabMaps;
 
-        private Vector3 prefabSize;
-        
+        private Vector3 _prefabSize;
+
         private void Awake()
         {
-            prefabSize = orePrefabMaps[0].prefab.GetComponent<SpriteRenderer>().bounds.size;
-            
+            _prefabSize = orePrefabMaps[0].prefab.GetComponent<SpriteRenderer>().bounds.size;
+
             _game = new Game();
-            
-            Debug.Log("Initial game: ");
-            Debug.Log(_game.ToString());
 
             var oreColumns = _game.Board.OreColumns;
             for (var i = 0; i < oreColumns.Count; i++)
@@ -27,7 +26,7 @@ namespace DefaultNamespace
                 var oreColumn = oreColumns[i];
 
                 var columnHolder = InstantiateColumn(oreColumn);
-                columnHolder.transform.localPosition = new Vector3(i * prefabSize.x, 0, 0);
+                columnHolder.transform.localPosition = new Vector3(i * _prefabSize.x, 0, 0);
             }
         }
 
@@ -40,7 +39,7 @@ namespace DefaultNamespace
                 var oreInstance =
                     Instantiate(Array.Find(orePrefabMaps, map => map.oreType == oreColumn.Get(j).type).prefab,
                         columnHolder.transform, false);
-                oreInstance.transform.localPosition = new Vector3(0, j * prefabSize.y, 0);
+                oreInstance.transform.localPosition = new Vector3(0, j * _prefabSize.y, 0);
             }
 
             return columnHolder;
@@ -49,13 +48,59 @@ namespace DefaultNamespace
         public void AddColumn()
         {
             _game.Board.AddColumn();
-            
-            Debug.Log("Added column: ");
-            Debug.Log(_game.ToString());
 
-            var oreColumn = _game.Board.OreColumns[_game.Board.OreColumns.Count - 1];
-            var columnHolder = InstantiateColumn(oreColumn);
-            columnHolder.transform.localPosition = new Vector3(-1 * prefabSize.x, 0, 0);
+            var createdOreColumn = _game.Board.OreColumns[_game.Board.OreColumns.Count - 1];
+            var columnHolder = InstantiateColumn(createdOreColumn);
+            columnHolder.transform.localPosition = new Vector3(-1 * _prefabSize.x, 0, 0);
+
+            var transformAccessArray = new TransformAccessArray(_game.Board.OreColumns.Count);
+
+            for (int i = 0; i < spawnPoint.transform.childCount; i++)
+            {
+                transformAccessArray.Add(spawnPoint.transform.GetChild(i));
+            }
+
+            new MoveColumnsLeftJob
+                {
+                    PrefabWidth = _prefabSize.x
+                }
+                .Schedule(transformAccessArray)
+                .Complete();
+
+            transformAccessArray.Dispose();
+        }
+
+        [BurstCompile]
+        private struct MoveColumnsLeftJob : IJobParallelForTransform
+        {
+            public float PrefabWidth;
+
+            public void Execute(int index, TransformAccess transform)
+            {
+                transform.position += Vector3.left * PrefabWidth;
+            }
+        }
+
+        [BurstCompile]
+        public struct MoveColumnsRightJob : IJobParallelForTransform
+        {
+            public float prefabWidth;
+
+            public void Execute(int index, TransformAccess transform)
+            {
+                transform.position += Vector3.right * prefabWidth;
+            }
+        }
+
+        [BurstCompile]
+        public struct MoveOresDownJob : IJobParallelForTransform
+        {
+            public float prefabHeight;
+
+            public void Execute(int index, TransformAccess transform)
+            {
+                transform.position += Vector3.down * prefabHeight;
+            }
         }
     }
 }
